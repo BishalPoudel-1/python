a1a260d8b6410ac5bb37102498fdbb304b772d11
BishalPoudel-1 <120388416+BishalPoudel-1@users.noreply.github.com>
2022-12-28 20:06:44 +0545

    Add files via upload

---
+++ Calculate the multiplication and sum of two numbers.py
@@ -0,0 +1,7 @@
+#Calculate the multiplication and sum of two numbers.
+number1=int(input("enter the number1: "))
+number2=int(input("enter the number2: "))
+sum=number1+number2
+multi=number1*number2
+print(number1,"+", number2,"=",sum)
+print(number1,"*", number2,"=",multi)
